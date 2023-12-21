5c67acd931cdfd9eeb4ec2786688e3330365c2f7
Christian Lee <christianlee4037@gmail.com>
2023-12-21 01:18:11 -0800

    Workout log data is pulled from db and is displayed in correct format

--- client/src/components/WorkoutLogItem.jsx
+++ client/src/components/WorkoutLogItem.jsx
@@ -1,9 +1,17 @@
 import React, { useRef, useState, useEffect } from "react";
 
 const WorkoutLogItem = ({ workout }) => {
+  const convertDate = () => {
+    const date = new Date(workout.date);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    });
+  };
   return (
     <div className="workout-item-container">
-      <p className="workout-item-date">{workout.date}</p>
+      <p className="workout-item-date">{convertDate()}</p>
       <div>
         <h3 className="workout-item-routine">{workout.routine}</h3>
         {workout.exercises.map((exercise) => (
