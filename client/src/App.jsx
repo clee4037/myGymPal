570e68b364fe7c02e0beb4c23192818afbed7158
Christian Lee <christianlee4037@gmail.com>
2023-12-23 02:38:07 -0800

    Added Calendar View

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,13 @@ import NavBar from "./components/NavBar.jsx";
 import NewWorkout from "./components/NewWorkout.jsx";
 import WorkoutLog from "./components/WorkoutLog.jsx";
 import "./App.css";
-// import History from "./components/History.jsx";
 import Routines from "./components/Routines.jsx";
+
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
 
 function App() {
   const [page, setPage] = useState("log");
@@ -14,6 +19,15 @@ function App() {
   return (
     <div className="App">
       <NavBar className="mb-10" currentPage={page} updatePage={updatePage} />
+      <div>
+        <Calendar
+          localizer={localizer}
+          // events={myEventsList}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+        />
+      </div>
       {page === "routine" && <Routines />}
       {page === "workout" && <NewWorkout updatePage={updatePage} />}
       {page === "log" && <WorkoutLog updatePage={updatePage} />}
