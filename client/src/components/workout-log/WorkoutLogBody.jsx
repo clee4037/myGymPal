2a5f55660398d51f9d5c6dcf2b39e02103311119
Christian Lee <christianlee4037@gmail.com>
2024-01-30 16:07:46 -0800

    Reorganize workout log components

--- client/src/components/workout-log/WorkoutLogBody.jsx
+++ client/src/components/workout-log/WorkoutLogBody.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import ListView from "./ListView";
-import CalendarView from "./CalendarView";
+import ListView from "../log-list-view/ListView";
+import CalendarView from "../log-calendar-view/CalendarView";
 import { fetchWorkouts } from "../../utils/slice/logSlice";
 
 const WorkoutLogBody = () => {
--- client/src/containers/WorkoutLog.jsx
+++ client/src/containers/WorkoutLog.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import WorkoutLogBody from "../components/workout-log/WorkoutLogBody";
-
-import WorkoutLogLeft from "../components/workout-log/WorkoutLogLeft";
-import WorkoutLogRight from "../components/workout-log/WorkoutLogRight";
-import "../stylesheets/workout_log.css";
+import WorkoutLogHeader from "../components/workout-log/WorkoutLogHeader";
 
 const WorkoutLog = () => {
   return (
     <>
-      <div className="flex flex-row justify-between pl-5 pr-5 items-center">
-        <WorkoutLogLeft />
-        <WorkoutLogRight />
-      </div>
+      <WorkoutLogHeader />
       <WorkoutLogBody />
     </>
   );
